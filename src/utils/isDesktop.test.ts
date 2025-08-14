9c67f992774a803bd9f6e5cbea03f80e0e2a9b03
André Marinho <107189254+andre-lmarinho@users.noreply.github.com>
2025-08-13 22:39:28 -0300

    test: restore window width in isDesktop test

--- src/utils/isDesktop.test.ts
+++ src/utils/isDesktop.test.ts
@@ -1,6 +1,19 @@
 import isDesktop from './isDesktop';
 
 describe('isDesktop', () => {
+  let originalInnerWidth: number;
+
+  beforeEach(() => {
+    originalInnerWidth = window.innerWidth;
+  });
+
+  afterEach(() => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      value: originalInnerWidth,
+    });
+  });
+
   it('returns true when width is at least 768px', () => {
     Object.defineProperty(window, 'innerWidth', { writable: true, value: 800 });
     expect(isDesktop()).toBe(true);
