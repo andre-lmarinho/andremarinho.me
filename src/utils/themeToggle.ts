faaec310b3e4146595232f8b7929ec090d559178
André Marinho <107189254+andre-lmarinho@users.noreply.github.com>
2025-09-20 09:45:16 -0300

    refactor: inline theme toggle state update (#96)

--- src/utils/themeToggle.ts
+++
@@ -1,8 +0,0 @@
-export interface ThemeToggleOptions {
-  darkMode: boolean;
-  setDarkMode: (value: boolean) => void;
-}
-
-export default function themeToggle({ darkMode, setDarkMode }: ThemeToggleOptions) {
-  setDarkMode(!darkMode);
-}
