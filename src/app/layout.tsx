671ae476262afa39150b65c641261d9be872bfee
André Marinho <andre_lmarinho@hotmail.com>
2025-09-20 23:05:48 -0300

    add Analytics and Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import './globals.css';
 import Layout from '@/components/layout';
 import cn from '@/utils';
 import { Providers } from './providers';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = localFont({
   src: [
@@ -61,6 +63,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Providers>
           <Layout>{children}</Layout>
         </Providers>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
