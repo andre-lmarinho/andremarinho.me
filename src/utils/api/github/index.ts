6ec813b6ca38d2d2564c2a8f9b6d1d192fea8092
andre-lmarinho <andre_lmarinho@hotmail.com>
2025-12-20 12:10:52 -0300

    fix: avoid undefined github token

--- src/utils/api/github/index.ts
+++ src/utils/api/github/index.ts
@@ -1,17 +1,24 @@
 import callApi, { type callApiOptions } from '@/utils/api/callApi';
 import { transformRepositories, transformUserInformation } from './mutators';
 
-const githubApiClient = <Response, Data>(options: callApiOptions<Response, Data>) =>
-  callApi({
+const githubApiClient = <Response, Data>(options: callApiOptions<Response, Data>) => {
+  const githubToken = process.env.GITHUB_API_TOKEN;
+  const headers: Record<string, string> = {
+    Accept: 'application/vnd.github.v3+json',
+  };
+
+  if (githubToken) {
+    headers.Authorization = `Bearer ${githubToken}`;
+  }
+
+  return callApi({
     mutator: options.mutator,
     url: `https://api.github.com${options.url}`,
     requestOptions: {
-      headers: {
-        Accept: 'application/vnd.github.v3+json',
-        Authorization: `Bearer ${process.env.GITHUB_API_TOKEN}`,
-      },
+      headers,
     },
   });
+};
 
 export const fetchRepositories = () =>
   githubApiClient({
