1ac1db5bf4a4fb03971741dde2db9b1a57279c38
andre-lmarinho <andre_lmarinho@hotmail.com>
2025-12-20 10:45:16 -0300

    chore(msw): add mock service worker and handlers

--- src/utils/api/github/index.ts
+++ src/utils/api/github/index.ts
@@ -1,24 +1,17 @@
 import callApi, { type callApiOptions } from '@/utils/api/callApi';
 import { transformRepositories, transformUserInformation } from './mutators';
 
-const githubApiClient = <Response, Data>(options: callApiOptions<Response, Data>) => {
-  const token = process.env.GITHUB_API_TOKEN;
-
-  if (!token) {
-    throw new Error('Missing GITHUB_API_TOKEN. Set it to fetch GitHub data.');
-  }
-
-  return callApi({
+const githubApiClient = <Response, Data>(options: callApiOptions<Response, Data>) =>
+  callApi({
     mutator: options.mutator,
     url: `https://api.github.com${options.url}`,
     requestOptions: {
       headers: {
         Accept: 'application/vnd.github.v3+json',
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${process.env.GITHUB_API_TOKEN}`,
       },
     },
   });
-};
 
 export const fetchRepositories = () =>
   githubApiClient({
