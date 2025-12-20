48331c1d139ede0d9f6027b7c1a04ef3d21ad5d2
andre-lmarinho <andre_lmarinho@hotmail.com>
2025-12-20 10:45:01 -0300

    chore(mdx): remove duplicate mdx components file

--- src/components/mdx-components.tsx
+++
@@ -1,9 +0,0 @@
-import type { MDXComponents } from 'mdx/types';
-import { TextLink } from '@/components/TextLink';
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    TextLink,
-    ...components,
-  };
-}
